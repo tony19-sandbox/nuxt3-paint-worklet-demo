8074dd6d9718d73879f74928fbbd4ac9ff56960c
Tony Trinh <tony19@gmail.com>
2021-10-25 14:49:33 -0500

    demo houdini-noise paint worklet

--- app.vue
+++ app.vue
@@ -1,6 +1,28 @@
 <template>
   <div>
-    <!-- Remove this component to get started! -->
-    <NuxtWelcome />
+    <button>Click</button>
   </div>
 </template>
+
+<script>
+import houdiniNoise from './node_modules/houdini-noise/dist/worklet.js?url'
+
+export default {
+  mounted() {
+    CSS.paintWorklet.addModule(houdiniNoise)
+  }
+}
+</script>
+
+<style scoped>
+button {
+  --noise: 0;
+  --noise-cell-size: 1;
+  --noise-hue: 10;
+  --noise-saturation: 80;
+  --noise-lightness: 10 100;
+  background: paint(noise);
+  width: 100px;
+  height: 2rem;
+}
+</style>
